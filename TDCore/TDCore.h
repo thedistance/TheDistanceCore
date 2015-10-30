46683a4b4ee9fb38fd3a2f939ab0f40bc23386ea
Josh Campion <joshcampion89@gmail.com>
2015-10-30 13:05:39 +0000

    Initial Commit

---
+++ TDCore/TDCore.h
@@ -0,0 +1,19 @@
+//
+//  TDCore.h
+//  TDCore
+//
+//  Created by Josh Campion on 30/10/2015.
+//  Copyright © 2015 The Distance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDCore.
+FOUNDATION_EXPORT double TDCoreVersionNumber;
+
+//! Project version string for TDCore.
+FOUNDATION_EXPORT const unsigned char TDCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDCore/PublicHeader.h>
+
+
