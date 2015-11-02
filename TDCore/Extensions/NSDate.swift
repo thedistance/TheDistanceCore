53f1ea396864f7f7f4579822fa3b498399f31abd
Josh Campion <joshcampion89@gmail.com>
2015-11-02 12:53:45 +0000

    - Updated NSDateComponents to support .TimeZone and .Calendar - Added Date & DateComponents tests.

--- TDCore/Extensions/NSDate.swift
+++ TDCore/Extensions/NSDate.swift
@@ -57,3 +57,27 @@ public func >=(d1:NSDate, d2:NSDate) -> Bool {
 public func <=(d1:NSDate, d2:NSDate) -> Bool {
     return d1.compare(d2) != .OrderedDescending
 }
+
+/**
+ 
+ Convenience function for comparing two `NSDate` objects. `isEqual(_:)` on `NSDate` is overriden to use this method.
+ 
+ - note: `compareDate(_:toDate:toUnitGranularity:)` should be used for more complex comparing.
+ 
+ - returns: `true` if `d1.compare(d2)` is `.OrderedSame`.
+ */
+public func ==(d1:NSDate, d2:NSDate) -> Bool {
+    return d1.compare(d2) == .OrderedSame
+}
+
+extension NSDate {
+    
+    override public func isEqual(object: AnyObject?) -> Bool {
+    
+        guard let other = object as? NSDate else {
+            return false
+        }
+        
+        return self == other
+    }
+}
